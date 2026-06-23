b7f98da23e093c946171bfe485ea332165dc453e
Suman Osti <107532348+metalhead224@users.noreply.github.com>
2026-06-23 11:45:21 -0400

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+        int newArrLength = 2 * n;
+        int[] ans = new int[newArrLength];
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        
+        return ans;
+    }
+}
