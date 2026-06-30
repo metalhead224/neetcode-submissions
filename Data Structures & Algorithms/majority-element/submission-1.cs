3ad61215a6bc75224bf55c8bbc4f464362e6eed9
Suman Osti <107532348+metalhead224@users.noreply.github.com>
2026-06-30 14:35:19 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+
+            count += (candidate == num) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
